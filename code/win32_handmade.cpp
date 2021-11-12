c1ac39afcc3914c710fecce6cfd849b0c97c7bff
Tessa Isola <tessag159@icloud.com>
2021-11-11 20:08:48 -0600

    Day 7. Initialized DirectSound

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdint.h>
 #include <xinput.h>
+#include <dsound.h>
 
 #define internal static
 #define local_persist static
@@ -10,6 +11,7 @@ typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
+typedef int32 bool32;
 
 typedef uint8_t uint8;
 typedef uint16_t uint16;
@@ -62,7 +64,7 @@ internal void Win32LoadXInput(void)
 {
   // TODO (Tess): Test on Windows Eight
   HMODULE XInputLibrary = LoadLibraryA("xinput1_4.dll");
-  
+
   if(!XInputLibrary)
     {
       HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
@@ -74,6 +76,89 @@ internal void Win32LoadXInput(void)
       XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
       if(!XInputSetState) { XInputSetState = XInputSetStateStub; }
     }
+}
+
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPGUID pcGuiDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
+
+
+internal void Win32InitDSound(HWND Window, int32 BufferSize, int32 SamplesPerSecond)
+{
+  HMODULE DSoundLibrary = LoadLibraryA("dsound.dll");
+  if(DSoundLibrary)
+    {
+      direct_sound_create *DirectSoundCreate = (direct_sound_create *)GetProcAddress(DSoundLibrary, "DirectSoundCreate");
+
+      // TODO (Tess): Double check if this works on xp - DirectSound 8 or 7??
+      LPDIRECTSOUND DirectSound;
+      if(DirectSoundCreate && SUCCEEDED(DirectSoundCreate(0, &DirectSound, 0)))
+	{
+	  WAVEFORMATEX WaveFormat = {};
+
+	  WaveFormat.wFormatTag = WAVE_FORMAT_PCM;
+	  WaveFormat.nChannels = 2;
+	  WaveFormat.nSamplesPerSec = SamplesPerSecond;
+	  WaveFormat.wBitsPerSample = 16;
+	  WaveFormat.nBlockAlign = (WaveFormat.nChannels*WaveFormat.wBitsPerSample) / 8;
+	  WaveFormat.nAvgBytesPerSec = WaveFormat.nBlockAlign*WaveFormat.nSamplesPerSec;
+	  WaveFormat.cbSize = 0;
+
+	  if(SUCCEEDED(DirectSound->SetCooperativeLevel(Window, DSSCL_PRIORITY)))
+	    {
+	      DSBUFFERDESC BufferDescription = {};
+	      BufferDescription.dwSize = sizeof(BufferDescription);
+	      BufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+	      // TODO (Tess): DSBCAPS_GLOBALFOCUS?
+	      LPDIRECTSOUNDBUFFER PrimaryBuffer;
+	      if(SUCCEEDED(DirectSound->CreateSoundBuffer(&BufferDescription, &PrimaryBuffer, 0)))
+		{
+		  if(SUCCEEDED(PrimaryBuffer->SetFormat(&WaveFormat)))
+		    {
+		      OutputDebugStringA("Primary buffer format was set.\n");
+		      // NOTE (Tess): We finally set the format of the buffer!
+		    }
+		  else
+		    {
+		      // TODO (Tess): Diagnostic
+		    }
+		}
+	      else
+		{
+		  // TODO (Tess): Diagnostic
+		}
+	    }
+	  else
+	    {
+	      // TODO (Tess): Diagnostic
+	    }
+
+	  // NOTE (Tess): "Create" a secondary buffer
+
+	  // TODO (Tess): DSBCAPS_GETCURRENTPOSITION2
+	  DSBUFFERDESC BufferDescription = {};
+	  BufferDescription.dwSize = sizeof(BufferDescription);
+	  BufferDescription.dwFlags = 0;
+	  BufferDescription.dwBufferBytes = BufferSize;
+	  BufferDescription.lpwfxFormat = &WaveFormat;
+	  LPDIRECTSOUNDBUFFER SecondaryBuffer;
+
+	  if(SUCCEEDED(DirectSound->CreateSoundBuffer(&BufferDescription, &SecondaryBuffer, 0)))
+	    {
+	      OutputDebugStringA("Secondary buffer created successfully!\n");
+	      // NOTE (Tess): Start it playing!
+	    }
+	  else
+	    {
+	      // TODO (Tess): Diagnostics
+	    }
+	}
+      else
+	{
+	  // TODO (Tess): Diagnostic
+	}
+    }
+
 }
 
 internal win32_window_dimension Win32GetWindowDimension(HWND Window)
@@ -130,7 +215,7 @@ internal void Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, i
 
   Buffer->BytesPerPixel = 4;
   int BitmapMemorySize = (Buffer->Width*Buffer->Height)*Buffer->BytesPerPixel;
-  Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+  Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
 
   // TODO (Tess): Probably clear this to black.
   Buffer->Pitch = Width*Buffer->BytesPerPixel;
@@ -222,7 +307,7 @@ internal LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPA
 	      {
 	      }
 	  }
-	bool AltKeyWasDown = ((LParam & (1 << 29)) != 0);
+	bool32 AltKeyWasDown = ((LParam & (1 << 29)) != 0);
 	if((VKCode = VK_F4) && AltKeyWasDown)
 	  {
 	    GlobalRunning = false;
@@ -270,6 +355,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 			CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
       if (Window)
 	{
+	  Win32InitDSound(Window, 48000, 48000*sizeof(int16)*2);
 	  // NOTE(Me): Since we specified CS_OWNDC, we can just get the DC once since we are not sharing it.
 	  HDC DeviceContext = GetDC(Window);
 
@@ -316,7 +402,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 		      int16 StickX = Pad->sThumbLX;
 		      int16 StickY = Pad->sThumbLY;
 
-		      
+
 		      XOffset += StickX >> 12;
 		      YOffset += StickY >> 12;
 		    }
